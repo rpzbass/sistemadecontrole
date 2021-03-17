cd33895529eb828e68405285dcce50139fbf1ee6
rpźbass <ronaldop.tech@gmail.com>
2021-03-17 01:55:56 -0300

    Inclusão Classe Pedido

--- SistemadeCadastro/src/entidades/Pedido.java
+++ SistemadeCadastro/src/entidades/Pedido.java
@@ -1,5 +1,55 @@
 package entidades;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Pedido {
 
+	private Long id;
+	private Date data;
+	private Double valorTotal;
+	
+	Set<PedidoItem> itens = new HashSet<PedidoItem>();
+	
+	public Long getId() {
+	
+		return id;
+	
+	}
+	
+	public void setId(Long id) {
+	
+		this.id = id;
+	
+	}
+	
+	public Date getData() {
+	
+		return data;
+	
+	}
+	
+	public void setData(Date data) {
+	
+		this.data = data;
+	
+	}
+	
+	public Double getValorTotal() {
+	
+		return valorTotal;
+	
+	}
+	
+	public void setValorTotal(Double valorTotal) {
+	
+		this.valorTotal = valorTotal;
+	
+	}
+	
+	
+	
+	
+	
 }
